2998e3e32b714dded6293c37734a2d436265b98a
dkflfkd53 <b57062562@gmail.com>
2025-04-26 22:41:50 +0900

    리팩

--- src/main/java/com/founderz/authorizationservertest/controller/AuthController.java
+++ src/main/java/com/founderz/authorizationservertest/controller/AuthController.java
@@ -1,17 +1,26 @@
 package com.founderz.authorizationservertest.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import com.founderz.authorizationservertest.controller.dto.SignupRequest;
+import com.founderz.authorizationservertest.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public void signup(@RequestBody SignupRequest request) {
+        userService.signup(request);
+    }
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
-
 }
-
